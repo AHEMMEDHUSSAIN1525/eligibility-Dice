b7485d23fc7ad617fb6a9839af7875d962ee11ec
Ahemmed <chinturockzz0494@gmail.com>
2020-04-03 19:28:25 +0530

    03-04-2020

---
+++ Even-odd.js
@@ -0,0 +1,8 @@
+function OddEven(){
+    var num = document.getElementById('num').value;
+    if ( num % 2 == 0) {
+        document.getElementById('Even').innerHTML = num + ' is a Even number';
+    }else{
+        document.getElementById('Odd').innerHTML = num + ' is a Odd number';
+    }
+} 
